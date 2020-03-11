4eec4a5adc68204b8e051aeed27e9dda968bded1
Becky Wu <becky.wu@twotalltotems.com>
2020-03-11 11:51:44 -0700

    Added Amplitude Range.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import PieChart from './pie-chart'
 import BarChart from './bar-chart'
 import PercentageBar from './percentage-bar'
 import InfluenceBar from './influence-bar'
+import AmplitudeRange from './amplitude-range'
 
 export {
     PieChart,
     BarChart,
     PercentageBar,
-    InfluenceBar
+    InfluenceBar,
+    AmplitudeRange
 }
